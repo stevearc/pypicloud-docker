6c6ed4689a03bbf58180fb45bb27b72b14335211
Steven Arcangeli <stevearc@stevearc.com>
2018-06-25 09:12:50 -0700

    Fix #19: Cannot set user in baseimages

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ branch="$(git branch --no-color | grep \* | cut -d ' ' -f2)"
 for py in py2 py3; do
   for base in alpine baseimage; do
     cd "$py-$base"
-    docker build . -t "stevearc/pypicloud:$branch-$py-$base"
+    docker build . -t "stevearc/pypicloud:$branch-$py-$base" -t "stevearc/pypicloud:local-$py-$base"
     cd ..
   done
 done
--- py3-alpine/pypicloud-uwsgi.sh
+++ py3-alpine/pypicloud-uwsgi.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/sbin/setuser pypicloud uwsgi --die-on-term /etc/pypicloud/config.ini
+if [ -z "$UWSGI_USER" ] || [ "$UWSGI_USER" = "root" ]; then
+  uwsgi --die-on-term /etc/pypicloud/config.ini
+else
+  /sbin/setuser "$UWSGI_USER" uwsgi --die-on-term /etc/pypicloud/config.ini
+fi
