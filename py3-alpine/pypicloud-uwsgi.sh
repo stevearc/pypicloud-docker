b7bf6548522853eb27322db5e3227efee043a677
Steven Arcangeli <stevearc@stevearc.com>
2018-06-26 09:15:47 -0700

    Remove pypicloud-uwsgi.sh from alpine dirs

--- py3-alpine/pypicloud-uwsgi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -z "$UWSGI_USER" ] || [ "$UWSGI_USER" = "root" ]; then
-  uwsgi --die-on-term /etc/pypicloud/config.ini
-else
-  /sbin/setuser "$UWSGI_USER" uwsgi --die-on-term /etc/pypicloud/config.ini
-fi
